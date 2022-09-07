ace7f65720626400f4606f6be487ecedbdff2287
Neven Dimač <neven.dimac@gmail.com>
2022-09-07 22:53:02 +0200

    Create unit test based on requirements

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RoomBookingApp.Core.Tests
+{
+    internal class RoomBookingRequestProcessor
+    {
+        public RoomBookingRequestProcessor()
+        {
+        }
+
+        public RoomBookingResult BookRoom(RoomBookingRequest bookingRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
